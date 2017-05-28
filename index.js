ac181b73eeb29bd9889f38330bf1a96ed9c04bc0
Yizhen Zhao <andy@nodeswork.com>
2017-05-27 21:02:40 -0700

    Fix no content generated error.

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ nodeswork
     server:       'http://localhost:28888',
     port:         28900,
     appletId:     '592281f962a5ed86bda7c5b9',
+    appletToken:  'huoSposJhHaxCa0z'
   })
   .withComponent(nodeswork.Messager)
   .process(async function(ctx) {
     logger.info("Processing hello world request.");
-    var quote = await randomQuote(),
+    var quote = (await randomQuote())[0],
         textContent = htmlToText.fromString(quote.content),
         messager = ctx.components.messager;
 
+    logger.info("Get random quote:", quote);
     messager.sendMessage({
-      message: textContent + '  BY  ' + quote.title
+      message: textContent + '\n\n--By  ' + quote.title
     });
   })
   .start(function() {
